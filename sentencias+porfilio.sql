6f307e3dfbd0d93c409b0da3cc10a1b1935ceb7d
agustinaporfilio <139725587+agustinaporfilio@users.noreply.github.com>
2023-08-13 21:56:20 -0300

    Add files via upload

---
+++ sentencias+porfilio.sql
@@ -0,0 +1,13 @@
+USE mysql;
+
+-- Creacion de 2 usuarios nuevos
+CREATE USER 'user1@localhost';
+
+CREATE USER 'user2@localhost';
+
+-- Otorgar permisos de solo lectura de la DB al user1
+GRANT SELECT ON Gimnasios.* TO 'user1@localhost';
+
+-- Otorgar permisos de lectura, insercion y modificacion de datos de la DB al user2
+GRANT SELECT, INSERT, UPDATE ON Gimnasios.* TO 'user2@localhost';
+
